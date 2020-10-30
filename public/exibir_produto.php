2ce9290dbc2063bceacae435ff4d1a7eb8b069b0
Vitor Souz <vytorsouzadev@gmail.com>
2020-10-30 17:30:40 -0200

    ADD UPDATE -> Update FrontEnd -> RegisterProduct and See Product

--- public/exibir_produto.php
+++ public/exibir_produto.php
@@ -6,61 +6,73 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Exibir Produto</title>
+        <title>Produto: <?=strtoupper($dadosProduto[0]->nome_produto)?> </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            section{
-                width: 70%;
-                margin: auto;
-                font-family:arial;
+        <link rel="stylesheet" href="exibirProduto.css">
+        <script>
+            function imagem(url){
+                //receber o id da imagem e setar ela no lugar da imagem principal
+                const img = document.querySelector("#imgProduto");
+                img.src=url;
             }
-            h1{
-                color: rgba(0,0,0,.8);
-                border-bottom: 1px solid rgba(0,0,0,.1);
-                height:60px;
-                line-height: 70px;
-                margin-bottom: 40px;
-            }
-            #imagens {
-                background-color: red;
-            }
-            .caixa-img{
-                width: 15%;
-                float: left;
-                padding: 1%;
-                background-color: rgb(123,104,238,.4);
-                margin: 10px;
-                height: 150px;
-                cursor:pointer;
-            }
-            img{
-                width: 100%;
-            }
-            p{
-                width: 70%;
-                text-align: justify;
-                line-height: 30px;
-            }
-
-        </style>
+        </script>
     </head>
     <body>
-        <section>
-        <div>
-            <h1><?=strtoupper($dadosProduto[0]->nome_produto)?></h1>
-            <p><b>Descrição: </b><?=ucwords($dadosProduto[0]->descricao)?></p>
-        </div>
-            <?foreach($imgProduto as $item){?>
-                <?$diretorioIMG="./../app/src/img/"?>
-                <div id="imagens">
-                        <div class="caixa-img">
-                            <img src="<?=$diretorioIMG?><?=$item->nome_IMAGEM?>">
+
+
+
+    <div id="container-dad">
+            
+            <!-- NavBar -->
+            <div id="navbar" class="container border-bt">
+                <h2>Produto</h2>
+            </div>
+            <!-- Botao Listar Produtos -->
+            <a class="container botao-verProduto" href="listar_produtos.php"><h2>Todos os Produtos</h2></a>
+            
+            <div id="container-produtos" class=" container">
+                <!-- Card Geral -->
+                <div class="container containerPdt">
+                    <!-- Card da Imagens -->
+                    <div class="cardImagem container">
+
+
+                        <!-- Imagem Pricipal do Produto -->
+                        <div class="imagemProduto">
+                            <?$diretorioIMG="./../app/src/img/"?>
+                            
+                            <img id="imgProduto" src="<?=$diretorioIMG="./../app/src/img/"?><?=$imgProduto[0]->nome_IMAGEM?>" alt="" >
                         </div>
+
+                        <!-- Imagens do Produtos -->
+                        <div class="imgensPequenas ">
+                            <?$diretorioIMG="./../app/src/img/"?>
+
+                            <?foreach($imgProduto as $item){?>
+                                <?$diretorioIMG="./../app/src/img/"?>
+                                <img  onCLick="imagem('<?=$diretorioIMG?><?=$item->nome_IMAGEM?>')" src="<?=$diretorioIMG?><?=$item->nome_IMAGEM?>" alt="">
+
+                            <?}?>
+
+
+                        </div>
+                    </div>
+                    <div class="container" ><br/></div>
+                    <div id="" class=" container nomeProduto">
+                        <h2> <a href="#4"><?=strtoupper($dadosProduto[0]->nome_produto)?></a></h2>
+                    </div>
+                    <div class="container" ><br/></div>
+                    <div id="" class=" container descricaoProduto">
+                        <h2>Descrição</h2>
+                        <p>
+                            <?=ucwords($dadosProduto[0]->descricao)?>
+                        </p>
+                    </div>
+                    
                 </div>
 
-            <?}?>
-            
-        </section>
+        </div>
+
     </body>
 </html>
